048b0bfcc1da6850c75a3ea7a4cacd72c04f76e8
Sebastian Grans <sebastian.grans@zivid.com>
2025-04-28 01:38:10 +0200

    Fix bug in `test_bool.py`

--- tests/test_bool.py
+++ tests/test_bool.py
@@ -17,7 +17,7 @@ log = logging.getLogger(__name__)
 )
 def test_single_bool(value: bool, step: SequenceCallStep):
     string_measurement = step.add_test_step(f"Single Boolean Limit Step [{value}]", SingleBooleanLimitStep)
-    result = string_measurement.add_result(value)
+    result = string_measurement.add_result(result=value)
     log.info(f"{value} = {result}")
 
 
@@ -25,15 +25,15 @@ def test_single_bool(value: bool, step: SequenceCallStep):
     ("names", "values"),
     [(["First", "Second"], [True, False])],
 )
-def test_multiple_string_limits(
+def test_multiple_bool_limits(
     names: list[str],
     values: list[bool],
     step: SequenceCallStep,
 ):
     numeric_measurement = step.add_test_step(
-        "Multiple String Limit Step",
+        "Multiple Bool Limit Step",
         MultipleBooleanLimitStep,
     )
     for name, value in zip(names, values, strict=True):
-        result = numeric_measurement.add_result(name, value)
+        result = numeric_measurement.add_result(name, result=value)
         log.info(f"{name}: {value} = {result}")
